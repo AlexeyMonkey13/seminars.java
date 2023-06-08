e2ad976007c6053b5d809c6a9377a7636f83dd86
AlexeyMonkey13 <Aptuct89@inbox.ru>
2023-06-09 01:45:32 +0700

    first task

--- tasks.java
+++ tasks.java
@@ -1,6 +1,10 @@
+import java.util.Random;
+import java.util.random.RandomGenerator;
 public class tasks {
     public static void main(String[] args) {
-        String i = "Hello";
+//1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
+        Random rand = new Random();
+        int i = rand.nextInt(0, 2000);
         System.out.println(i);
     }
 }
